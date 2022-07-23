2c36e8f53408596d7036584caf8909f21b97da3f
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2022-07-23 21:50:46 +0200

    Add cucumber to project

---
+++ src/test/java/kata/hygge/RunCucumberTest.java
@@ -0,0 +1,13 @@
+package kata.hygge;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  plugin = {"pretty", "html:target/cucumber-report.html"},
+  features = {"src/test/resources"}
+)
+public class RunCucumberTest {
+}
